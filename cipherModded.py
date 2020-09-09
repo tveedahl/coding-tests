77729789807a6554d6a81908118660832959af98
Tim Veedahl <timveedahl@gmail.com>
2020-09-09 09:11:50 -0500

    Updated cipherModded.py

--- cipherModded.py
+++ cipherModded.py
@@ -2,16 +2,18 @@ import sys
 
 # Prompt user for line of text
 userTxt = input("Enter a line of text:")
+print(userTxt)
+sys.exit()
 
 # Prompt user for a shift value and check for exceptions
 try:
     userShiftVal = int(input("Enter an integer between 1 and 25:"))
     if 1 <= userShiftVal <= 25:
-        # print(userTxt, userShiftVal)
+        print(userTxt, userShiftVal)
         for i in userTxt:
             # Find character unicode value
             iUni = ord(i)
-            print(iUni + " ")
+            # print(iUni + " ")
     else:
         sys.exit("Entered value is not in range. Goodbye")
 except ValueError:
