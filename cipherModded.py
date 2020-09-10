7facf44aac55885e79751ed469d7610dd27cc2f3
Tim Veedahl <timveedahl@gmail.com>
2020-09-10 09:52:35 -0500

    Updated cipherModded.py

--- cipherModded.py
+++ cipherModded.py
@@ -1,20 +1,27 @@
 import sys
 
-# Prompt user for line of text
-userTxt = input("Enter a line of text:")
-print(userTxt)
-sys.exit()
-
-# Prompt user for a shift value and check for exceptions
 try:
+    # Prompt user for line of text
+    userTxt = input("Enter a line of text:")
+
+    # Prompt user for a shift value and check for exceptions
     userShiftVal = int(input("Enter an integer between 1 and 25:"))
+
+    # Encrypt user entered text using user entered shift value
+    cipher = ''
     if 1 <= userShiftVal <= 25:
-        print(userTxt, userShiftVal)
         for i in userTxt:
-            # Find character unicode value
-            iUni = ord(i)
-            # print(iUni + " ")
+            # If i is whitespace
+            if i == ' ':
+                cipher = cipher + i
+            # If i is uppercase
+            elif i.isupper():
+                cipher = cipher + chr((ord(i) + userShiftVal - 65) % 26 + 65)
+            # If i is otherwise
+            else:
+                cipher = cipher + chr((ord(i) + userShiftVal - 97) % 26 + 97)
+        print(cipher)
     else:
         sys.exit("Entered value is not in range. Goodbye")
 except ValueError:
-    print("The value you entered is not valid")
+    sys.exit("The value you entered is not valid")
