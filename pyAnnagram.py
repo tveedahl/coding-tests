5c8b2ade6ebee123a62c1416feb5add719d22c8f
tveedahl <tveedahl@gmail.com>
2020-09-17 09:22:45 -0500

    Added/Updated pyAnnagram.py

--- pyAnnagram.py
+++ pyAnnagram.py
@@ -3,11 +3,11 @@ import sys
 phraseOne = input('Enter a phrase or word: ')
 phraseTwo = input('Enter a second phrase or word to see if it is an annagram of the first: ')
 
-print('The first phrase or word you entered is: ' + phraseOne)
-print('The second phrase or word you entered is: ' + phraseTwo)
+print('The first phrase or word you entered was: ' + phraseOne)
+print('The second phrase or word you entered was: ' + phraseTwo)
 
 # Check if user input is empty
-if phraseOne is None or phraseTwo is None:
+if not phraseOne or not phraseTwo:
     print("One or more of phrases or words contains no text. Goodbye")
     sys.exit()
 else:
@@ -19,11 +19,9 @@ else:
     phraseOneLower = phraseOneStripped.lower()
     phraseTwoLower = phraseTwoStripped.lower()
 
-    print(phraseOneLower, phraseTwoLower)
-
-    """# Compare phrases or words to verify they are annagrams
-    if phraseOneLower == phraseTwoLower:
-        print(phraseOne + " and" + phraseTwo + " are anagrams and contain the same characters!")
+    # Compare phrases or words to verify they are annagrams
+    if sorted(phraseOneLower) == sorted(phraseTwoLower):
+        print(phraseOne + " and " + phraseTwo + " are anagrams and contain the same characters!")
     else:
         print("The phrases or words you entered are not anagrams and don't contain the same characters. Goodbye")
-        sys.exit()"""
+        sys.exit()
