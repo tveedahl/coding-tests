922649ac383e8ae9d0ef3f84afbe508a8914847a
tveedahl <tveedahl@gmail.com>
2020-11-29 11:39:36 -0600

    Added file io readline demonstration functionality

--- fileIoTest.py
+++ fileIoTest.py
@@ -5,20 +5,20 @@ class FileIo:
 	# Illustrate usage of opening file read stream
 	def readSomeTxt(self):
 		try:
-			cnt = 0
-			s = open('text.txt', "rt")
-			ch = s.read(1)
-			while ch != '':
-				print(ch, end='')
-				cnt += 1
-				ch = s.read(1)
+			ccnt = lcnt = 0
+			s = open('text.txt', 'rt')
+			line = s.readline()
+			while line != '':
+				lcnt += 1
+				for ch in line:
+					print(ch, end='')
+					ccnt += 1
+				line = s.readline()
 			s.close()
-			print("\n\nCharacters in file:", cnt)
+			print("\n\nCharacters in file:", ccnt)
+			print("Lines in file:     ", lcnt)
 		except IOError as e:
-			print("I/O error occurred: ", strerr(e.errno))
-
-	# Read text file by line via file stream
-	def readTxtByLine(self):
+			print("I/O error occurred:", strerr(e.errno))
 
 # Instantiate fileTester
 fileTester = FileIo()
