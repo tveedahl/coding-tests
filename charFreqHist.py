1861aa38e2f054adb8e2dd3d1f7f72ad69c365b2
tveedahl <tveedahl@gmail.com>
2020-12-14 08:56:15 -0600

    Updated charFreqHist.py

--- charFreqHist.py
+++ charFreqHist.py
@@ -1,17 +1,19 @@
 from os import strerror
 
 class CharFreqHist():
-
-    def usrFilePrompt(self):
-        global fileName = input("Enter the name of a file to get input from: ")
-        return fileName
     
     def readAndCount(self):
         try:
-            aFile = self.usrFilePrompt()
-            aFileHandle = open(aFile, 'rb')
+            aFileName = input("Enter the name of a file to get input from: ")
+            fileContents = open(aFileName, 'r')
+            latCharCount = 0
+            for line in fileContents:
+                lowerLine = line.lower()
+                for char in lowerLine:
+                    print(char)
+            fileContents.close()
         except IOError:
-            print("Could not read file: ", aFile)
+            print("Could not read file: ", aFileName)
 
 impCharFreqHist = CharFreqHist()
 impCharFreqHist.readAndCount()
