c442ae5e078d75410bda1389fb460769f4308d41
sirommit <tveedahl@gmail.com>
2021-01-05 15:06:48 -0600

    Updated studentEval.py

--- studentEval.py
+++ studentEval.py
@@ -16,6 +16,7 @@ class StudentEval():
             fileHndl = open(aFileName, 'r')
             studentEvalDict = []
             fileLines = fileHndl.readlines()
+            studentCount = 0
             for line in fileLines:
                 lineSplt = line.split(" ")
                 rplPoints = float(lineSplt[2].replace("\n", ""))
@@ -27,9 +28,10 @@ class StudentEval():
                 #print(student[0], student[1])
                 if student[0] not in studentEvalDict and student[1] not in studentEvalDict:
                     studentEvalDict.append(student)
-                else:
-                    print(student[0], student[1])
-            print(studentEvalDict)
+                    if student[0] == studentEvalDict[0][0] and student[1] == studentEvalDict[0][1]:
+                        studentEvalDict[studentCount][2] = studentEvalDict[studentCount][2] + studentEvalDict[studentCount][2]
+                studentCount = studentCount + 1
+                print(studentEvalDict)
         except IOError:
             pass
 
