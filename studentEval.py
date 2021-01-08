be4e701d1bced39b949144806ab4648cbcbb72f0
sirommit <tveedahl@gmail.com>
2021-01-08 11:08:31 -0600

    Updated studentEval.py

--- studentEval.py
+++ studentEval.py
@@ -15,42 +15,38 @@ class StudentEval:
     def readAndSum(self):
         try:
             global aFileName
-            aFileName = input("Enter the name of a text file to get input from: ")
+            aFileName = input("Enter the name of a text file to get input from:")
             fileHndl = open(aFileName, 'r')
-            studentEvalList = []
+            studentEvalIntl = set()
+            studentEvalSummed = set()
             nameList = []
             fileLines = fileHndl.readlines()
             count = 0
             for line in fileLines:
                 lineSplt = line.split(" ")
                 rplPoints = float(lineSplt[2].replace("\n", ""))
-                studentIntl = [
+                studentIntl = (
                     lineSplt[0],
                     lineSplt[1],
                     rplPoints
-                ]
+                )
                 names = [
                     lineSplt[0],
                     lineSplt[1]
                 ]
                 if names not in nameList:
                     nameList.append(names)
-                    studentEvalList.append(studentIntl)
+                    studentEvalIntl.add(studentIntl)
                 else:
-                    stuEvalSum = rplPoints + studentEvalList[count][2]
-                    studentSummed = [
+                    # stuEvalSum = rplPoints + studentEvalIntl(count)[2]
+                    """studentSummed = (
                         lineSplt[0],
                         lineSplt[1],
                         stuEvalSum
-                    ]
-                    if lineSplt[0] not in studentEvalList:
-                        studentEvalList.append(studentSummed)
-                        # studentEvalList.pop(count)
+                    )
+                    studentEvalSummed.add(studentSummed)"""
                     count += 1
-            stuCount = 0
-            while stuCount < len(studentEvalList):
-                print(studentEvalList[stuCount][0], studentEvalList[stuCount][1], studentEvalList[stuCount][2])
-                stuCount += 1
+            # print(studentEvalIntl)
         except IOError:
             print("No file by that name exists in your current directory")
 
