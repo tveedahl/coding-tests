a75a360deb387fc530415e7d91b54413fe072732
tveedahl <tveedahl@gmail.com>
2021-01-13 10:28:07 -0600

    Updated studentEval.py

--- studentEval.py
+++ studentEval.py
@@ -17,36 +17,40 @@ class StudentEval:
             global aFileName
             aFileName = input("Enter the name of a text file to get input from:")
             fileHndl = open(aFileName, 'r')
-            studentEvalIntl = set()
-            studentEvalSummed = set()
+            studentEvalIntl = {}
+            studentEvalSummed = {}
             nameList = []
             fileLines = fileHndl.readlines()
             count = 0
             for line in fileLines:
                 lineSplt = line.split(" ")
                 rplPoints = float(lineSplt[2].replace("\n", ""))
-                studentIntl = (
-                    lineSplt[0],
-                    lineSplt[1],
-                    rplPoints
-                )
+                studentEvalIntl = {
+                    count: {
+                        "first": lineSplt[0],
+                        "last": lineSplt[1],
+                        "points": rplPoints
+                    }
+                }
                 names = [
                     lineSplt[0],
                     lineSplt[1]
                 ]
                 if names not in nameList:
                     nameList.append(names)
-                    studentEvalIntl.add(studentIntl)
+                    # studentEvalIntl.add(studentIntl)
                 else:
-                    # stuEvalSum = rplPoints + studentEvalIntl(count)[2]
-                    """studentSummed = (
-                        lineSplt[0],
-                        lineSplt[1],
-                        stuEvalSum
-                    )
-                    studentEvalSummed.add(studentSummed)"""
+                    stuEvalSum = rplPoints + studentEvalIntl[count]["points"]
+                    studentEvalSummed = {
+                        count: {
+                            "first": lineSplt[0],
+                            "last": lineSplt[1],
+                            "points": stuEvalSum
+                        }
+                    }
+                    # studentEvalSummed.add(studentSummed)
                     count += 1
-            # print(studentEvalIntl)
+            # print(studentEvalSummed)
         except IOError:
             print("No file by that name exists in your current directory")
 
